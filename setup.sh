4c0d59f4208be082bdb5de7cbb7dacce7ba41d04
Amit Kapadia <amit@zooniverse.org>
2013-09-29 14:45:07 -1000

    update setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 mkdir -vp data
-curl -k "http://astrojs.s3.amazonaws.com/sample/L1448_13CO.fits" -o "data/L1448_13CO.fits"
+
+curl -k "http://astrojs.s3.amazonaws.com/sample/L1448_13CO.fits.gz" -o "data/L1448_13CO.fits.gz"
+gunzip data/L1448_13CO.fits.gz
+
+curl -k "http://astrojs.s3.amazonaws.com/sample/eso149-g003.HI-cube.fits.gz" -o "data/eso149-g003.HI-cube.fits.gz"
+gunzip data/eso149-g003.HI-cube.fits.gz
